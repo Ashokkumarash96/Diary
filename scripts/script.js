a21a4e394a6e6716a5bb5feb4d0a118984be1b2f
Ashokkumar <ashokkumarash96@gmail.com>
2023-05-20 13:35:17 +0530

    comments update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-// Grab the entryForm element
 const entryForm = document.getElementById("entryForm");
 const entriesSection = document.querySelector("#entries");
 const entryTextbox = document.querySelector(".entry-textbox");
@@ -6,22 +5,27 @@ const entriesNav = document.querySelector(".entries-nav");
 
 let entries = 0;
 
-// handler to deal with button click
+// Function to handle form submission
 const handleForm = (e) => {
   event.preventDefault();
 
+  // Create a new div for the entry
   const entryDiv = document.createElement("div");
   entryDiv.className = "single-entry";
   entryDiv.innerHTML = entryTextbox.value;
   entryDiv.style.display = "none";
   entriesSection.appendChild(entryDiv);
 
+  // Clear the entry textbox
   entryTextbox.value = "";
+
+  // Create a button to display the entry
   const displayEntryButton = document.createElement("button");
   displayEntryButton.className = "display-entry-button";
   displayEntryButton.innerText = ++entries;
   entriesNav.appendChild(displayEntryButton);
 
+  // Event listener for displaying the entry when the button is clicked
   displayEntryButton.addEventListener("click", function () {
     const allEntries = document.querySelectorAll(".single-entry");
     allEntries.forEach((element) => (element.style.display = "none"));
@@ -29,5 +33,5 @@ const handleForm = (e) => {
   });
 };
 
-// Add an event listener
+// Event listener for form submission
 entryForm.addEventListener("submit", handleForm);
